821b1c3c8d4aa214f7e216187198a8e85b150353
Vedant Shinde <vedantve@gmail.com>
2026-06-13 19:54:16 -0700

    DE-1: Stable fixes

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
+from argparse import ArgumentParser
+
 from src.config.indicator_loader import load_indicators
 from src.ingestion.world_bank_ingestor import WorldBankIngestor
 from src.db.snowflake_client import SnowflakeClient
 
 
-START_YEAR = 1960
-END_YEAR = 2000
+parser = ArgumentParser()
+
+parser.add_argument(
+    "--start-year",
+    type=int,
+    required=True
+)
+
+parser.add_argument(
+    "--end-year",
+    type=int,
+    required=True
+)
+
+args = parser.parse_args()
+
+START_YEAR = args.start_year
+END_YEAR = args.end_year
 
 indicators = load_indicators()
 
@@ -32,18 +50,17 @@ for indicator in indicators:
         END_YEAR
     )
 
-    snowflake_client.insert_raw_payload(
-        indicator_code,
-        payload
-    )
-
     rows_loaded = len(payload[1])
 
-    snowflake_client.record_success(
+    snowflake_client.save_ingestion_run(
         indicator_code,
         START_YEAR,
         END_YEAR,
+        payload,
         rows_loaded
     )
 
-    print(f"Loaded {indicator['name']}")
+    print(
+        f"Loaded {indicator['name']} "
+        f"({START_YEAR}-{END_YEAR})"
+    )
